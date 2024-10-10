494120e5e614637bce09d6adeca287814ef32fab
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-10 16:51:28 +0200

    Add schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,7 @@
-from pydantic import BaseModel
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel, Field
 
 
 class BadSequence(BaseModel):
@@ -12,6 +15,67 @@ class MismatchingSequences(BaseModel):
     sequence2: str
 
 
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+
+
 class TuningParameters(BaseModel):
     slow_speed_threshold: float
     conservation_threshold: float
+    sequence_table_mapping: dict[str, str]
+
+
+class User(BaseModel):
+    id: UUID
+    creation_date: datetime
+    email: str
+    # password: str
+
+
+class UserForm(BaseModel):
+    email: str
+    password: str
+
+
+class CodonTable(BaseModel):
+    name: str
+    user_id: UUID | None
+    creation_date: datetime
+    organism: str
+    custom: bool
+
+
+class CodonTableForm(BaseModel):
+    name: str
+    user_id: UUID | None
+    organism: str
+    custom: bool
+
+
+class CodonTranslation(BaseModel):
+    codon_table_name: str
+    codon: str
+    anti_codon: str
+    amino_acid: str
+    trna_gcn: float
+    corresponding_codon: str
+    wobble_rate: float
+
+
+class Result(BaseModel):
+    id: UUID
+    user_id: UUID | None
+    creation_date: datetime
+    host_organism: str
+    mode: str
+    parameters: TuningParameters
+
+
+class TunedSequence(BaseModel):
+    id: UUID
+    result_id: UUID
+    name: str
+    input: str
+    output: str
+    percentage_identity: float
