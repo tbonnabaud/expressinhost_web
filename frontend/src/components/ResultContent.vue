f902ce51b3b4a26492786f1eb90d13eb849df863
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-20 21:54:21 +0200

    Add result content component

---
+++ frontend/src/components/ResultContent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { TuningOutput } from '@/lib/interfaces'
+import { computed } from 'vue'
+
+const props = defineProps<TuningOutput>()
+
+const modeLabelMapping: Record<string, string> = {
+  direct_mapping: 'Direct mapping',
+  optimisation_and_conservation_1: 'Optimisation and conservation 1',
+  optimisation_and_conservation_2: 'Optimisation and conservation 2',
+}
+
+const hostCondonTable = props.result.host_codon_table_name
+const mode = computed(
+  () => modeLabelMapping[props.result.mode] || 'Unknown mode',
+)
+</script>
+
+<template>
+  <h1>Expression in {{ hostCondonTable }}</h1>
+
+  <p>{{ mode }}</p>
+
+  <pre>{{ JSON.stringify(tuned_sequences, null, 2) }}</pre>
+</template>
