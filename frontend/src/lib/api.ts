db02b61a0965979ca87fdaf5a456116f139167d6
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-20 19:51:39 +0200

    API service

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,22 @@
+import axios, { type AxiosResponse } from 'axios'
+import type { RunTrainingForm, TuningOutput } from './interfaces'
+
+const client = axios.create({
+  baseURL: '/api',
+})
+
+function parseResponse(response: AxiosResponse) {
+  if (response.status == 200) {
+    return response.data
+  } else {
+    console.error(response.status, response.data)
+    return null
+  }
+}
+
+export const API = {
+  runTraining: async (form: RunTrainingForm) => {
+    const response = await client.post('/run-tuning', form)
+    return parseResponse(response) as TuningOutput
+  },
+}
