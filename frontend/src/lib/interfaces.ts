06780455c59922d9966d4468133755bedf834204
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-20 19:51:54 +0200

    Add interfaces for output

--- frontend/src/lib/interfaces.ts
+++ frontend/src/lib/interfaces.ts
@@ -13,3 +13,24 @@ export interface CodonTable {
   organism: string
   custom: boolean
 }
+
+export interface Result {
+  creation_date: string
+  host_codon_table_name: string
+  sequences_native_codon_tables: Record<string, string>
+  mode: string
+  slow_speed_threshold: number
+  conservation_threshold?: number
+}
+
+export interface TunedSequence {
+  name: string
+  input: string
+  output: string
+  identity_percentage: number
+}
+
+export interface TuningOutput {
+  result: Result
+  tuned_sequences: TunedSequence
+}
