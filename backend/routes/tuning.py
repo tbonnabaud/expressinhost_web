77f33e36dba4716a97443eb0c004712873eb7279
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-20 21:29:33 +0200

    Prepare case for user_id not None

--- backend/routes/tuning.py
+++ backend/routes/tuning.py
@@ -1,22 +1,22 @@
 import re
-from uuid import UUID
 
 from fastapi import APIRouter
 
 from ..core.codon_tables import process_codon_table_from_file
 from ..core.sequence_tuning import run_tuning
 from ..core.utils import find_organism_from_nucleotide_name
-from ..crud.results import ResultRepository
-from ..crud.tuned_sequences import TunedSequenceRepository
+
+# from ..crud.results import ResultRepository
+# from ..crud.tuned_sequences import TunedSequenceRepository
 from ..custom_types import SessionDependency
 
 # from ..models import CodonTranslation, Result
-from ..schemas import RunTuningForm
+from ..schemas import RunTuningForm, TuningOutput
 
 router = APIRouter(tags=["Tuning"])
 
 
-@router.post("/run-tuning", response_model=UUID)
+@router.post("/run-tuning", response_model=TuningOutput)
 def launch_tuning(session: SessionDependency, form: RunTuningForm):
     sequence_names = re.findall(
         r"^\> *(.*\w)", form.nucleotide_file_content, re.MULTILINE
@@ -36,7 +36,7 @@ def launch_tuning(session: SessionDependency, form: RunTuningForm):
         form.slow_speed_threshold,
     )
 
-    output = run_tuning(
+    tuned_sequences = run_tuning(
         form.nucleotide_file_content,
         form.clustal_file_content,
         native_codon_tables,
@@ -45,20 +45,27 @@ def launch_tuning(session: SessionDependency, form: RunTuningForm):
         form.conservation_threshold,
     )
 
-    result_id = ResultRepository(session).add(
-        {
-            "user_id": None,
-            "host_codon_table_name": form.host_codon_table_name,
-            "sequences_native_codon_tables": form.sequences_native_codon_tables,
-            "mode": form.mode,
-            "slow_speed_threshold": form.slow_speed_threshold,
-            "conservation_threshold": form.conservation_threshold,
-        }
-    )
+    result = {
+        "host_codon_table_name": form.host_codon_table_name,
+        "sequences_native_codon_tables": form.sequences_native_codon_tables,
+        "mode": form.mode,
+        "slow_speed_threshold": form.slow_speed_threshold,
+        "conservation_threshold": form.conservation_threshold,
+    }
 
-    for seq in output:
-        seq["result_id"] = result_id
+    user_id = None
 
-    TunedSequenceRepository(session).add_batch(output)
+    if user_id:
+        result["user_id"] = user_id
+        # result_id = ResultRepository(session).add(result)
 
-    return result_id
+        # # Attach the tuned sequences to the result
+        # for seq in tuned_sequences:
+        #     seq["result_id"] = result_id
+
+        # TunedSequenceRepository(session).add_batch(tuned_sequences)
+
+    return {
+        "result": result,
+        "tuned_sequences": tuned_sequences,
+    }
