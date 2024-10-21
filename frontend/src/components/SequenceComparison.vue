f980b35956d43ced23c3b70380ae9cb2757a299b
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-21 15:07:16 +0200

    Add color background to modified codons

--- frontend/src/components/SequenceComparison.vue
+++ frontend/src/components/SequenceComparison.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
-// import { ref } from 'vue';
+import { computed } from 'vue'
 import type { TunedSequence } from '@/lib/interfaces'
 
-defineProps<TunedSequence>()
+const props = defineProps<TunedSequence>()
+
+const seqComparison = computed(() => colorSequences(props.input, props.output))
+
+function colorSequences(inputSequence: string, outputSequence: string) {
+  const inputCodonArray = inputSequence.match(/.{3}/g) || []
+  const outputCodonArray = outputSequence.match(/.{3}/g) || []
+
+  return [
+    inputCodonArray
+      .map((item, i) => {
+        if (item == outputCodonArray[i]) {
+          return `<span class="in-seq">${item}</span>`
+        }
+        return item
+      })
+      .join(''),
+    outputCodonArray
+      .map((item, i) => {
+        if (item == inputCodonArray[i]) {
+          return `<span class="out-seq">${item}</span>`
+        }
+        return item
+      })
+      .join(''),
+  ]
+}
 </script>
 
 <template>
@@ -11,27 +37,23 @@ defineProps<TunedSequence>()
   <p><strong>-> Similarity:</strong> {{ identity_percentage.toFixed(2) }}%</p>
 
   <div class="sequence-group">
-    <div class="sequence" v-html="input"></div>
-    <div class="sequence" v-html="output"></div>
+    <div class="sequence" v-html="seqComparison[0]"></div>
+    <div class="sequence" v-html="seqComparison[1]"></div>
   </div>
+
+  <hr />
 </template>
 
 <style scoped>
 .sequence {
-  height: 40px;
+  height: 3em;
   white-space: nowrap;
+  font-family: monospace;
+  font-size: 1.5em;
 }
 
 .sequence-group {
   overflow-x: scroll;
   margin: 2em 0;
-}
-
-.in {
-  background-color: brown;
-}
-
-.out {
-  background-color: green;
 }
 </style>
