6ccccd4b6efee7c6611d0154528aef3434bbe55f
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-22 10:55:49 +0200

    Add commented line, just in case

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -7,6 +7,7 @@ import ResultView from '../views/ResultView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
