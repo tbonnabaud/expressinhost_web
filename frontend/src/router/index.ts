6bde374d8268e9b86e859b7753daed93659ab7bf
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-22 12:58:16 +0200

    Add not found view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -42,6 +42,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: () => import('../views/NotFoundView.vue'),
+    },
   ],
 })
 
