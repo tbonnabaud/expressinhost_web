12c1c24f3d4a4c8c34b55459a90b3e55d62e5ba3
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-23 22:17:02 +0200

    Add missing await and rename to makeRequest to avoid confusion with client.request

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -5,19 +5,21 @@ const client = axios.create({
   baseURL: '/api',
 })
 
-/**
- * The function `request` sends an asynchronous HTTP request using Axios and handles errors
- * appropriately.
- * @param {AxiosRequestConfig} config - The `config` parameter in the `request` function is of type
- * `AxiosRequestConfig`, which is an interface defining the configuration for an Axios HTTP request. It
- * typically includes properties like `url`, `method`, `headers`, `params`, `data`, etc., that are used
- * to customize
- * @returns The `request` function returns the data from the response if the request is successful. If
- * there is an error, it checks if the error is an Axios error with a response, and if so, it logs the
- * error message and response data. If it's a network error or the request failed, it logs a generic
- * error message. In both cases, it returns `null`.
- */
-async function request(config: AxiosRequestConfig) {
+// Interceptor to unify error handling
+// client.interceptors.response.use(
+//   response => response,
+//   error => {
+//     if (axios.isAxiosError(error) && error.response) {
+//       console.error(error.message, error.response.data)
+//     } else {
+//       console.error('Network error or request failed')
+//     }
+
+//     return Promise.reject(error)
+//   },
+// )
+
+async function makeRequest(config: AxiosRequestConfig) {
   try {
     const response = await client.request(config)
     return response.data
@@ -33,12 +35,12 @@ async function request(config: AxiosRequestConfig) {
 
 const REQUESTS = {
   get: async (url: string, params?: object) =>
-    request({ method: 'get', url, params }),
+    await makeRequest({ method: 'get', url, params }),
   post: async (url: string, data?: object) =>
-    request({ method: 'post', url, data }),
+    await makeRequest({ method: 'post', url, data }),
   put: async (url: string, data?: object) =>
-    request({ method: 'put', url, data }),
-  delete: async (url: string) => request({ method: 'delete', url }),
+    await makeRequest({ method: 'put', url, data }),
+  delete: async (url: string) => await makeRequest({ method: 'delete', url }),
 }
 
 export const API = {
