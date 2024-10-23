e166f5a4de6c80dc31d631597e3f34100c3b07af
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-23 10:57:22 +0200

    Fill home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,6 +2,19 @@
 
 <template>
   <main class="container">
-    <h1>Home</h1>
+    <!-- <h1>Home</h1> -->
+
+    <p>
+      Application for tuning the codon sequence of an mRNA for recombinant
+      protein expression in a host microorganism.
+    </p>
   </main>
 </template>
+
+<style scoped>
+p {
+  margin-top: 5em;
+  text-align: center;
+  font-size: 1.5em;
+}
+</style>
