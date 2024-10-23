283bd9668d84f76769befc76335f4f48724d9ee9
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-23 21:05:04 +0200

    Factorization

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -5,6 +5,14 @@ const client = axios.create({
   baseURL: '/api',
 })
 
+// client.interceptors.response.use(
+//   response => response,
+//   error => {
+//     console.error(error)
+//     return Promise.reject(error)
+//   },
+// )
+
 function parseResponse(response: AxiosResponse) {
   if (response.status == 200) {
     return response.data
@@ -14,9 +22,26 @@ function parseResponse(response: AxiosResponse) {
   }
 }
 
-export const API = {
-  runTraining: async (form: RunTrainingForm) => {
-    const response = await client.post('/run-tuning', form)
-    return parseResponse(response) as TuningOutput
+const REQUESTS = {
+  get: async (url: string, params?: object) => {
+    const response = await client.get(url, { params })
+    return parseResponse(response)
   },
+  post: async (url: string, data?: object) => {
+    const response = await client.post(url, data)
+    return parseResponse(response)
+  },
+  put: async (url: string, data?: object) => {
+    const response = await client.put(url, data)
+    return parseResponse(response)
+  },
+  delete: async (url: string) => {
+    const response = await client.delete(url)
+    return parseResponse(response)
+  },
+}
+
+export const API = {
+  runTraining: async (form: RunTrainingForm) =>
+    (await REQUESTS.post('/run-tuning', form)) as TuningOutput,
 }
