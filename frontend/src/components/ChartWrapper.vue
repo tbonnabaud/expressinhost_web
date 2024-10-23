31c461de1fe91173e1d25d544ac46e207444bb62
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-23 17:15:25 +0200

    Create wrapper around Chart.js

---
+++ frontend/src/components/ChartWrapper.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { ref, onMounted, onBeforeUnmount, defineProps } from 'vue'
+import {
+  Chart,
+  registerables,
+  type ChartOptions,
+  type ChartData,
+  type ChartType,
+} from 'chart.js'
+
+// Register Chart.js components
+Chart.register(...registerables)
+
+// Define props types
+interface ChartProps {
+  type: ChartType
+  data: ChartData
+  options?: ChartOptions
+}
+
+// Props for the chart data and options
+const props = defineProps<ChartProps>()
+
+// Reference to the canvas element
+const chartCanvas = ref<HTMLCanvasElement | null>(null)
+let chartInstance: Chart | null = null
+
+// Create the chart when the component is mounted
+onMounted(() => {
+  if (chartCanvas.value) {
+    chartInstance = new Chart(chartCanvas.value, {
+      type: props.type,
+      data: props.data,
+      options: props.options,
+    })
+  }
+})
+
+// Destroy the chart instance when the component is unmounted
+onBeforeUnmount(() => {
+  if (chartInstance) {
+    chartInstance.destroy()
+  }
+})
+</script>
+
+<template>
+  <canvas ref="chartCanvas"></canvas>
+</template>
