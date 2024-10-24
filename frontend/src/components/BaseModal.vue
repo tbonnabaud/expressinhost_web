83df3d9330ba9b45ae645b31e40c8f994503ab36
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-24 14:51:01 +0200

    Add smooth transition

--- frontend/src/components/BaseModal.vue
+++ frontend/src/components/BaseModal.vue
@@ -8,22 +8,34 @@ function close() {
 </script>
 
 <template>
-  <dialog :open="open" @click.self="close">
-    <article>
-      <header>
-        <button aria-label="Close" rel="prev" @click="close"></button>
-        <p>
-          <strong class="modal-title">{{ title }}</strong>
-        </p>
-      </header>
+  <Transition>
+    <dialog v-if="open" @click.self="close" open>
+      <article>
+        <header>
+          <button aria-label="Close" rel="prev" @click="close"></button>
+          <p>
+            <strong class="modal-title">{{ title }}</strong>
+          </p>
+        </header>
 
-      <slot></slot>
-    </article>
-  </dialog>
+        <slot></slot>
+      </article>
+    </dialog>
+  </Transition>
 </template>
 
 <style scoped>
 header .modal-title {
   font-size: 1.5em;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
