103573547248655a13e4adb58f3120d489d7e981
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-25 09:36:34 +0200

    Add lorem ipsum

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
   <main class="container">
     <h1>This is an about page</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, fugit
+      blanditiis nesciunt magni pariatur, natus soluta quaerat impedit commodi,
+      voluptatum accusamus. Fugiat, totam dolore excepturi unde suscipit
+      deleniti quos veritatis!
+    </p>
   </main>
 </template>
