2e94c2240263b475b0f63b852b772ca6435f8f27
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-28 19:26:04 +0100

    Add basic authentication

--- backend/authentication.py
+++ backend/authentication.py
@@ -1,4 +1,14 @@
+from typing import Annotated
+
 import bcrypt
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+from .crud.users import UserRepository
+from .custom_types import Session, SessionDependency
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def hash_password(password: str) -> str:
@@ -15,3 +25,37 @@ def check_password(password: str, hashed_password: str) -> bool:
     determine if they match.
     """
     return bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+
+def get_current_user(session: Session, token: str):
+    user = UserRepository(session).get_by_email(token)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return user
+
+
+def check_is_member(
+    session: SessionDependency, token: Annotated[str, Depends(oauth2_scheme)]
+):
+    return get_current_user(session, token)
+
+
+def check_is_admin(
+    session: SessionDependency, token: Annotated[str, Depends(oauth2_scheme)]
+):
+    user = get_current_user(session, token)
+
+    if user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not admin",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return user
--- backend/routes/users.py
+++ backend/routes/users.py
@@ -1,9 +1,17 @@
+from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 
-from ..authentication import hash_password
+from ..authentication import (
+    check_is_admin,
+    check_password,
+    get_current_user,
+    hash_password,
+    oauth2_scheme,
+)
 from ..crud.users import UserRepository
 from ..custom_types import SessionDependency
 from ..schemas import User, UserForm
@@ -11,7 +19,7 @@ from ..schemas import User, UserForm
 router = APIRouter(tags=["Users"])
 
 
-@router.get("/users", response_model=list[User])
+@router.get("/users", response_model=list[User], dependencies=[Depends(check_is_admin)])
 def list_users(session: SessionDependency):
     return UserRepository(session).list()
 
@@ -26,9 +34,17 @@ def get_user(session: SessionDependency, user_id: UUID):
     return user
 
 
-@router.post("users/login")
-def log_in_user(session: SessionDependency):
-    return {}
+@router.post("/token")
+def log_in_user(
+    session: SessionDependency,
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+):
+    user = UserRepository(session).get_by_email(form_data.username)
+
+    if not user or not check_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.email, "token_type": "bearer"}
 
 
 @router.post("/users", response_model=UUID)
@@ -40,11 +56,39 @@ def add_user(session: SessionDependency, data: UserForm):
     return UserRepository(session).add(user)
 
 
-@router.put("/users/{user_id}")
+@router.put("/users/me")
+def update_me(
+    session: SessionDependency,
+    token: Annotated[str, Depends(oauth2_scheme)],
+    data: UserForm,
+):
+    current_user = get_current_user(session, token)
+
+    updated_user = data.model_dump()
+    updated_user["hashed_password"] = hash_password(data.password)
+    del updated_user["password"]
+
+    return UserRepository(session).update(current_user.id, updated_user)
+
+
+@router.delete("/users/me")
+def delete_me(
+    session: SessionDependency, token: Annotated[str, Depends(oauth2_scheme)]
+):
+    current_user = get_current_user(session, token)
+
+    return UserRepository(session).delete(current_user.id)
+
+
+@router.put("/users/{user_id}", dependencies=[Depends(check_is_admin)])
 def update_user(session: SessionDependency, user_id: UUID, data: UserForm):
-    return UserRepository(session).update(user_id, data.model_dump())
+    updated_user = data.model_dump()
+    updated_user["hashed_password"] = hash_password(data.password)
+    del updated_user["password"]
+
+    return UserRepository(session).update(user_id, updated_user)
 
 
-@router.delete("/users/{user_id}")
+@router.delete("/users/{user_id}", dependencies=[Depends(check_is_admin)])
 def delete_user(session: SessionDependency, user_id: UUID):
     return UserRepository(session).delete(user_id)
