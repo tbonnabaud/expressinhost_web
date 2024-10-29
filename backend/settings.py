cf2ca5a0dc555bd76d46d04fd46a2e5d56c9663d
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-29 15:51:15 +0100

    Add JWT secret key and algorithm

--- backend/settings.py
+++ backend/settings.py
@@ -3,6 +3,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
+    # openssl rand -hex 32
+    JWT_SECRET_KEY: str = (
+        "26192e0eba1df7e20106f2a2d55a65f1dde175361103721dd4b747cfbb5f2ab5"
+    )
+    JWT_ALGORITHM: str = "HS256"
 
     model_config = SettingsConfigDict(env_file=".env")
 
