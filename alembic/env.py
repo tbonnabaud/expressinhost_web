1a8848ac8cc644a4af164af606ccffe1b1fe75b5
Thierry BLB <thierry.blabruyere@laposte.net>
2024-10-31 17:20:58 +0100

    Isort

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,8 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-
 from backend.database import DATABASE_URL
 from backend.models import Base
 
