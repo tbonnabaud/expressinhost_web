5f6a8963b70493b60469a3ed7dd9d58eddba4bed
Thierry BLB <thierry.blabruyere@laposte.net>
2024-11-01 18:14:13 +0100

    Add results pagination and count

--- backend/routes/results.py
+++ backend/routes/results.py
@@ -5,13 +5,28 @@ from fastapi import APIRouter
 from ..crud.results import ResultRepository
 from ..database import SessionDependency
 from ..schemas import Result
+from .common import FilterParamDependency
 
 router = APIRouter(tags=["Results"])
 
 
 @router.get("/users/{user_id}/results", response_model=list[Result])
-def list_user_results(session: SessionDependency, user_id: UUID):
-    return ResultRepository(session).list_from_user(user_id)
+def list_user_results(
+    session: SessionDependency,
+    filter_params: FilterParamDependency,
+    user_id: UUID,
+):
+    return ResultRepository(session).list_from_user(
+        user_id, filter_params.offset, filter_params.limit
+    )
+
+
+@router.get("/users/{user_id}/results/count", response_model=list[Result])
+def count_user_results(
+    session: SessionDependency,
+    user_id: UUID,
+):
+    return ResultRepository(session).count_from_user(user_id)
 
 
 @router.get("/results/{result_id}", response_model=Result)
