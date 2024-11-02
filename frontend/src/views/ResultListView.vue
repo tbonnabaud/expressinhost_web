176ac76a048f721a8568633077b4e0ff6e0f70c6
Thierry BLB <thierry.blabruyere@laposte.net>
2024-11-02 15:42:12 +0100

    Display results

--- frontend/src/views/ResultListView.vue
+++ frontend/src/views/ResultListView.vue
@@ -1,7 +1,70 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Result } from '@/lib/interfaces'
+import { onMounted, ref } from 'vue'
+import { RouterLink } from 'vue-router'
+import { API } from '@/lib/api'
+import { store } from '@/lib/store'
+
+const currentUser = store.currentUser
+const resultList = ref([] as Array<Result>)
+
+onMounted(async () => await fetchResultList())
+
+async function fetchResultList() {
+  if (currentUser.value) {
+    const [data, error] = await API.results.list(currentUser.value.id)
+
+    if (!error) {
+      resultList.value = data
+    }
+  }
+}
+</script>
 
 <template>
   <main class="container">
     <h1>Result list</h1>
+
+    <div class="grid-of-three">
+      <article
+        v-for="(result, index) in resultList"
+        :key="index"
+        class="zoomable"
+      >
+        <header>
+          <RouterLink :to="'/results/' + result.id" class="contrast">
+            <h4>{{ result.id }}</h4>
+          </RouterLink>
+        </header>
+
+        <p>{{ result.host_codon_table_name }}</p>
+      </article>
+    </div>
   </main>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .container {
+    padding-left: 15em;
+    padding-right: 15em;
+  }
+}
+
+.grid-of-three {
+  display: grid;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+}
+
+.grid-of-three > article {
+  margin: 0.5em;
+}
+
+.zoomable {
+  transition: transform 0.2s;
+}
+
+.zoomable:hover {
+  transform: scale(1.03);
+}
+</style>
--- frontend/src/views/ResultView.vue
+++ frontend/src/views/ResultView.vue
@@ -1,7 +1,48 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { API } from '@/lib/api'
+import { onMounted, ref } from 'vue'
+import ResultContent from '@/components/results/ResultContent.vue'
+import type { Result, TunedSequence } from '@/lib/interfaces'
+
+const props = defineProps<{ id: string }>()
+
+const result = ref({} as Result)
+const tunedSequences = ref([] as Array<TunedSequence>)
+
+onMounted(async () => {
+  result.value = await fetchResult(props.id)
+
+  if (result.value.id) {
+    tunedSequences.value = await fetchTunedSequences(result.value.id)
+  }
+})
+
+async function fetchResult(id: string) {
+  const [data, error] = await API.results.get(id)
+
+  return !error ? data : {}
+}
+
+async function fetchTunedSequences(resultId: string) {
+  const [data, error] = await API.tunedSequences.list(resultId)
+
+  return !error ? data : []
+}
+</script>
 
 <template>
   <main class="container">
     <h1>Result</h1>
+
+    <ResultContent :result="result" :tuned_sequences="tunedSequences" />
   </main>
 </template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .container {
+    padding-left: 15em;
+    padding-right: 15em;
+  }
+}
+</style>
