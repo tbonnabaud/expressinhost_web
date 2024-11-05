ddbbac338d70ffdbdc511e392481d53e3773ac55
Thierry BLB <thierry.blabruyere@laposte.net>
2024-11-05 18:08:46 +0100

    Get results for me

--- backend/routes/results.py
+++ backend/routes/results.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from fastapi import APIRouter
 
+from ..authentication import TokenDependency, get_current_user
 from ..crud.results import ResultRepository
 from ..database import SessionDependency
 from ..schemas import Result
@@ -10,30 +11,33 @@ from .common import FilterParamDependency
 router = APIRouter(tags=["Results"])
 
 
-@router.get("/users/{user_id}/results", response_model=list[Result])
+@router.get("/users/me/results", response_model=list[Result])
 def list_user_results(
     session: SessionDependency,
+    token: TokenDependency,
     filter_params: FilterParamDependency,
-    user_id: UUID,
 ):
+    current_user = get_current_user(session, token)
+
     return ResultRepository(session).list_from_user(
-        user_id, filter_params.offset, filter_params.limit
+        current_user.id, filter_params.offset, filter_params.limit
     )
 
 
-@router.get("/users/{user_id}/results/count", response_model=list[Result])
-def count_user_results(
-    session: SessionDependency,
-    user_id: UUID,
-):
-    return ResultRepository(session).count_from_user(user_id)
+@router.get("/users/me/results/count", response_model=list[Result])
+def count_user_results(session: SessionDependency, token: TokenDependency):
+    current_user = get_current_user(session, token)
+
+    return ResultRepository(session).count_from_user(current_user.id)
 
 
-@router.get("/results/{result_id}", response_model=Result)
-def get_result(session: SessionDependency, result_id: UUID):
-    return ResultRepository(session).get(result_id)
+@router.get("/users/me/results/{result_id}", response_model=Result)
+def get_result(session: SessionDependency, token: TokenDependency, result_id: UUID):
+    current_user = get_current_user(session, token)
+    return ResultRepository(session).get(current_user.id, result_id)
 
 
-@router.delete("/results/{result_id}")
-def delete_result(session: SessionDependency, result_id: UUID):
-    return ResultRepository(session).delete(result_id)
+@router.delete("/users/me/results/{result_id}")
+def delete_result(session: SessionDependency, token: TokenDependency, result_id: UUID):
+    current_user = get_current_user(session, token)
+    return ResultRepository(session).delete(current_user.id, result_id)
