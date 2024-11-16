b36098f2e62827fd3b86cefbefd8dc654cc75ec3
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-11-16 14:29:33 +0100

    Fetch data from API

--- frontend/src/components/codon-tables/CodonTableForm.vue
+++ frontend/src/components/codon-tables/CodonTableForm.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import { onMounted, reactive, ref } from 'vue'
+import { API } from '@/lib/api'
+import type { CodonTable } from '@/lib/interfaces'
 import PartialCodonTable from './PartialCodonTable.vue'
-import { reactive } from 'vue'
+import CodonTableSearchSelect from './CodonTableSearchSelect.vue'
+
+const codonTableList = ref([] as Array<CodonTable>)
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const table = reactive({
@@ -25,9 +30,21 @@ const table = reactive({
   Tyr: [],
   Val: [],
 })
+
+onMounted(async () => await fetchCodonTableList())
+
+async function fetchCodonTableList() {
+  const [data, error] = await API.codonTables.list()
+
+  if (!error) {
+    codonTableList.value = data
+  }
+}
 </script>
 
 <template>
+  <CodonTableSearchSelect id="codonTableSelect" :options="codonTableList" />
+
   <div class="row">
     <div class="column">
       <PartialCodonTable title="Alanine (Ala)" amino-acid="Ala" />
@@ -65,5 +82,10 @@ const table = reactive({
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 20px;
+}
+
+#codonTableSelect {
+  width: 50%;
+  margin-bottom: 2em;
 }
 </style>
