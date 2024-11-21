1861b190f747a330ddc989094272bf2f6562b0b3
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-11-21 13:42:34 +0100

    Restore fixed watchers

--- frontend/src/components/codon-tables/PartialCodonTableRow.vue
+++ frontend/src/components/codon-tables/PartialCodonTableRow.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-// import { watch } from 'vue'
+import { watch } from 'vue'
 import { CODON_LIST } from '@/lib/referentials'
 
 defineProps<{
@@ -11,28 +11,28 @@ const trnaGcn = defineModel('trnaGcn', { default: 1 })
 const correspCodon = defineModel('correspCodon', { default: 'OOO' })
 const wobbleRate = defineModel('wobbleRate', { default: 0 })
 
-// watch(trnaGcn, value => {
-//   if (value == 0) {
-//     correspCodon.value = 'GCU'
-//     wobbleRate.value = 0.35
-//   }
-// })
+watch(trnaGcn, value => {
+  if (value > 0) {
+    correspCodon.value = 'OOO'
+    wobbleRate.value = 0
+  }
+})
 
-// watch(correspCodon, value => {
-//   if (value == 'OOO') {
-//     wobbleRate.value = 0
-//   } else {
-//     trnaGcn.value = 0
-//   }
-// })
+watch(correspCodon, value => {
+  if (value == 'OOO') {
+    wobbleRate.value = 0
+  } else {
+    trnaGcn.value = 0
+  }
+})
 
-// watch(wobbleRate, value => {
-//   if (value == 0) {
-//     correspCodon.value = 'OOO'
-//   } else {
-//     trnaGcn.value = 0
-//   }
-// })
+watch(wobbleRate, value => {
+  if (value == 0) {
+    correspCodon.value = 'OOO'
+  } else {
+    trnaGcn.value = 0
+  }
+})
 </script>
 
 <template>
