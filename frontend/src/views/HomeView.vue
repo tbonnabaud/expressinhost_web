dfd29c72152017a487d8cee6baaa4b6906e0c219
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-11-21 18:07:01 +0100

    Add banner to home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,26 +5,58 @@ const user = store.currentUser
 </script>
 
 <template>
-  <main class="container">
-    <h1 v-if="user">Welcome {{ user.full_name }}</h1>
+  <main class="container-fluid">
+    <h1 v-if="user" id="welcome">Welcome {{ user.full_name }}</h1>
 
-    <p>
-      <strong>ExpressInHost</strong> tunes codon sequences for recombinant
-      protein expression in a host organism
-    </p>
+    <div class="image-container">
+      <img id="banner" src="../assets/banner.png" alt="Banner" />
 
-    <p>⚠ This web application is under development ⚠</p>
+      <p id="description">
+        <strong>ExpressInHost</strong> tunes codon sequences for recombinant
+        protein expression in a host organism
+      </p>
+    </div>
+
+    <p id="warning">⚠ This web application is under development ⚠</p>
   </main>
 </template>
 
 <style scoped>
-h1,
-p {
-  margin-top: 3em;
+.container-fluid {
+  padding-left: 0;
+  padding-right: 0;
+}
+
+#welcome {
+  margin-bottom: 1em;
   text-align: center;
 }
 
-p {
+.image-container {
+  margin-top: 2em;
+  position: relative;
+  text-align: center;
+}
+
+#banner {
+  width: 100%;
+}
+
+#description {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 3em;
+  backdrop-filter: blur(15px);
+  padding: 1em;
+  color: whitesmoke;
+  text-shadow: 0 0 5px #303030;
+}
+
+#warning {
+  margin-top: 1em;
   font-size: 2.5em;
+  text-align: center;
 }
 </style>
