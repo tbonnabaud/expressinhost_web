6189ad676c53cd391ac9aeee4b9d5183ad39fa81
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-11-26 09:44:37 +0100

    Various size fixes

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -11,13 +11,29 @@ const user = store.currentUser
     <div class="image-container">
       <img id="banner" src="../assets/banner.png" alt="Banner" />
 
-      <p id="description">
+      <p id="summary">
         <strong>ExpressInHost</strong> tunes codon sequences for recombinant
         protein expression in a host organism
       </p>
     </div>
 
     <p id="warning">⚠ This web application is under development ⚠</p>
+
+    <div id="description">
+      <!-- <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        maximus libero, vel facilisis ipsum malesuada nec. Mauris molestie ipsum
+        quis risus dignissim placerat. Phasellus at suscipit erat, sit amet
+        congue odio. Vivamus sed lorem vehicula, consectetur urna a, tempor
+        erat. Aenean convallis gravida orci, at pharetra elit tincidunt vel.
+        Integer vulputate viverra nisl sit amet facilisis. Aenean condimentum
+        dapibus metus non viverra. Maecenas sagittis sem ac sollicitudin
+        sollicitudin. Praesent iaculis varius sagittis. Etiam a accumsan risus.
+        Praesent viverra non eros sed commodo. Quisque arcu dolor, pellentesque
+        sed velit sit amet, fermentum blandit sem. Fusce blandit urna dapibus,
+        sodales erat ut, euismod nisl. Sed et placerat nulla.
+      </p> -->
+    </div>
   </main>
 </template>
 
@@ -42,21 +58,27 @@ const user = store.currentUser
   width: 100%;
 }
 
-#description {
+#summary {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  font-size: 3em;
+  font-size: 2.5em;
   backdrop-filter: blur(15px);
-  padding: 1em;
+  padding: 0.5em;
   color: whitesmoke;
   text-shadow: 0 0 5px #303030;
 }
 
 #warning {
   margin-top: 1em;
-  font-size: 2.5em;
+  font-size: 2em;
   text-align: center;
+}
+
+#description {
+  margin: 2em 5em;
+  font-size: 1.5em;
+  text-align: justify;
 }
 </style>
