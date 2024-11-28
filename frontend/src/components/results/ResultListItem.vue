0d7c26507a60a4ea66823ab7cef11c96ef7d4e68
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-11-28 17:31:39 +0100

    Display date, organism and codon table name instead of id

--- frontend/src/components/results/ResultListItem.vue
+++ frontend/src/components/results/ResultListItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { Result } from '@/lib/interfaces'
 import { MODE_LABEL_MAPPING } from '@/lib/referentials'
+import { formatToLocaleDateString } from '@/lib/helpers'
 
 defineProps<{ result: Result }>()
 </script>
@@ -9,7 +10,13 @@ defineProps<{ result: Result }>()
   <RouterLink :to="'/results/' + result.id" class="contrast">
     <article class="zoomable">
       <header>
-        <h4>{{ result.id }}</h4>
+        <h5>
+          <span class="creation-date">
+            {{ formatToLocaleDateString(result.creation_date) }}
+          </span>
+          &nbsp; <i>{{ result.host_codon_table.organism }}</i> -
+          {{ result.host_codon_table.name }}
+        </h5>
       </header>
 
       <p>
@@ -36,5 +43,15 @@ a {
 
 .zoomable:hover {
   transform: scale(1.03);
+}
+
+h5 {
+  margin: 0.5em auto;
+}
+
+.creation-date {
+  border: 1px solid var(--pico-color);
+  border-radius: 5px;
+  padding: 3px 5px;
 }
 </style>
