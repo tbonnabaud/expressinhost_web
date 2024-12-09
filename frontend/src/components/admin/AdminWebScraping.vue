4929b4abb313b048b9dc6f9f79a36dacbd0fd016
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-12-09 23:02:29 +0100

    Button for running web scraping from the interface

---
+++ frontend/src/components/admin/AdminWebScraping.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { API } from '@/lib/api'
+
+async function runWebScraping() {
+  await API.admin.runWebScraping()
+}
+</script>
+
+<template>
+  <button type="button" @click="runWebScraping">
+    Run scraping of Lowe Lab database
+  </button>
+</template>
--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import RequiredAdmin from '@/components/RequiredAdmin.vue'
 import RequiredAuth from '@/components/RequiredAuth.vue'
+import AdminWebScraping from '@/components/admin/AdminWebScraping.vue'
 import { store } from '@/lib/store'
 
 const user = store.currentUser
@@ -11,6 +12,8 @@ const user = store.currentUser
     <template v-if="user && user.role == 'admin'">
       <h1>Administration</h1>
       <hr />
+
+      <AdminWebScraping />
     </template>
 
     <RequiredAdmin v-else-if="user && user.role != 'admin'" />
