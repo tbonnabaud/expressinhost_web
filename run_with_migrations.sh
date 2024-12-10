a8f1ffc2570dc33a75d0dfd51dbf6732f0ccffcf
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-12-10 10:35:53 +0100

    Use nproc to set the number of workers

--- run_with_migrations.sh
+++ run_with_migrations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run migrations, then add example codon tables if none exists in database, finally run the server
-alembic upgrade head && python -m backend.add_example_tables && uvicorn backend.main:app --host 0.0.0.0 --workers 4
+alembic upgrade head && python -m backend.add_example_tables && uvicorn backend.main:app --host 0.0.0.0 --workers $(nproc)
