5000758f3df54fcffc01439f8f6111d4e24729a5
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2024-12-11 19:31:29 +0100

    Add tooltip component

---
+++ frontend/src/components/ToolTip.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tooltip">
+    <slot></slot>
+    <span class="tooltiptext">
+      <slot name="tooltip">Lorem ipsum</slot>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.tooltip {
+  position: relative;
+  display: inline-block;
+  /* cursor: help; */
+}
+
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: max-content;
+  max-width: 300px;
+  background-color: #56637f;
+  border: 2px solid #40454e;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 10px;
+  opacity: 0.9;
+
+  position: absolute;
+  z-index: 1;
+  top: -10px;
+  left: 105%;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+</style>
