c5c1e5ce6ec907032ef8248475aa6777862fa241
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-01-17 11:46:15 +0100

    Admin routes to get log file and backups

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, BackgroundTasks, Depends
+from fastapi.responses import FileResponse
 
 from ..authentication import check_is_admin
 from ..external_db_extractors.lowe_lab import lowe_state_monitor, run_scraping
@@ -18,3 +19,13 @@ async def run_web_scraping(background_tasks: BackgroundTasks):
 @router.get("/external-db/web-scraping/state")
 def get_web_scraping_state():
     return lowe_state_monitor
+
+
+@router.get("/log")
+def get_log_file():
+    return FileResponse("expressinhost.log")
+
+
+@router.get("/log/backup/{number}")
+def get_backup_log_file(number: int):
+    return FileResponse(f"expressinhost.log.{number}")
