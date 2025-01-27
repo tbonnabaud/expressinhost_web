6f791d7062dc3150ae7fcd6ed35c413619da8ea1
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-01-28 00:07:57 +0100

    Reset password form as component

---
+++ frontend/src/components/ResetPasswordForm.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { API } from '@/lib/api'
+import { checkPasswordConstraints } from '@/lib/checkers'
+import type { UserPasswordForm } from '@/lib/interfaces'
+import WithAlertError from '@/components/WithAlertError.vue'
+
+const props = defineProps<{ token?: string }>()
+const emit = defineEmits(['update'])
+
+const password = ref('')
+const passwordError = ref('')
+
+watch(password, password => {
+  passwordError.value = checkPasswordConstraints(password)
+})
+
+async function handleSubmit() {
+  const [, error] = await API.users.updatePassword({
+    reset_token: props.token,
+    password: password.value,
+  } as UserPasswordForm)
+
+  if (!error) {
+    emit('update')
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit">
+    <fieldset>
+      <WithAlertError :error="passwordError">
+        <label>
+          New password
+          <i id="passwordIndications">
+            (between 8 and 20 characters, with at least one letter and one
+            number)
+          </i>
+          <input type="password" v-model="password" required />
+        </label>
+      </WithAlertError>
+    </fieldset>
+
+    <input type="submit" value="Submit" :disabled="Boolean(passwordError)" />
+  </form>
+</template>
+
+<style scoped>
+#passwordIndications {
+  font-size: 80%;
+}
+</style>
