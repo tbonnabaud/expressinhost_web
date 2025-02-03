38fbd3f9279abc679a5f1166b49a18917cc7673d
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-02-03 17:19:07 +0100

    Add admin section useers

--- frontend/src/components/admin/AdminSectionUsers.vue
+++ frontend/src/components/admin/AdminSectionUsers.vue
@@ -1,7 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { API } from '@/lib/api'
+import type { User } from '@/lib/interfaces'
+import { onMounted, ref } from 'vue'
+import AdminSectionUsersItem from './AdminSectionUsersItem.vue'
+
+const userList = ref([] as Array<User>)
+
+onMounted(async () => await fetchUserList())
+
+async function fetchUserList() {
+  const [data, error] = await API.users.list()
+
+  if (!error) {
+    userList.value = data
+  }
+}
+</script>
 
 <template>
   <section>
     <h2>Management of users</h2>
+
+    <div id="userList">
+      <AdminSectionUsersItem
+        v-for="user in userList"
+        :user="user"
+        :key="user.id"
+      />
+    </div>
   </section>
 </template>
+
+<style scoped>
+@media (min-width: 768px) {
+  #userList {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 15px;
+  }
+}
+</style>
---
+++ frontend/src/components/admin/AdminSectionUsersItem.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import type { User } from '@/lib/interfaces'
+import { API } from '@/lib/api'
+import { ref } from 'vue'
+
+const props = defineProps<{ user: User }>()
+
+const ariaInvalid = ref(undefined as boolean | undefined)
+
+async function handleUpdate(event: Event) {
+  const target = event.target as HTMLInputElement
+  const newRole = target.value
+
+  const [, error] = await API.users.updateUserRole(props.user.id, {
+    role: newRole,
+  })
+
+  if (!error) {
+    ariaInvalid.value = false
+  } else {
+    ariaInvalid.value = true
+    alert('Failed to update the user role.')
+  }
+
+  await new Promise(resolve => setTimeout(resolve, 5000))
+  ariaInvalid.value = undefined
+}
+</script>
+
+<template>
+  <article>
+    <header>
+      {{ user.full_name }}
+      (<a :href="'mailto:' + user.email">{{ user.email }}</a
+      >)
+    </header>
+
+    <div class="actions">
+      <select
+        class="role-select"
+        :value="user.role"
+        @change="handleUpdate"
+        :aria-invalid="ariaInvalid"
+      >
+        <option value="member">member</option>
+        <option value="admin">admin</option>
+      </select>
+
+      <button class="danger">Delete</button>
+    </div>
+  </article>
+</template>
+
+<style scoped>
+@media (min-width: 768px) {
+  .actions {
+    padding-bottom: 0;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-gap: 15px;
+  }
+
+  p {
+    line-height: 3em;
+  }
+}
+
+button,
+select {
+  height: fit-content;
+  width: 100%;
+}
+
+header {
+  font-size: large;
+}
+</style>
