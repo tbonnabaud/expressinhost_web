7f41d4e1e812a48c239dc6b3b72e948a4c5b44db
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-03-13 15:58:12 +0100

    Add new lines

--- run_with_migrations.sh
+++ run_with_migrations.sh
@@ -2,13 +2,18 @@
 
 # Create logs directory
 mkdir -p logs
+
 # Run database migrations
 alembic upgrade head
+
 # Add example codon tables if none exists in database
 python -m backend.add_example_tables
-# Run Python RQ workers
+
+# Run Python RQ workers in background
 python -m backend.workers &
+
 # Run the periodic web scraping script in background 
 python -m backend.external_db_extractors.lowe_lab &
+
 # Run the server
 uvicorn backend.main:app --host 0.0.0.0 --workers $(nproc)
