9ffae20a1579f1138ca01dc68263636e5dbf6535
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-04-03 10:47:24 +0200

    Add typing

--- frontend/src/lib/referentials.ts
+++ frontend/src/lib/referentials.ts
@@ -1,4 +1,4 @@
-import type { CodonTranslation } from './interfaces'
+import type { CodonTranslation, RestrictionSite } from './interfaces'
 import { groupByAminoAcid } from './helpers'
 
 export const MODE_LABEL_MAPPING: Record<string, string> = {
@@ -96,7 +96,7 @@ export const UTR_EXAMPLE =
   'ACCCGGCGCTCCATTAAATAGCCGTAGACGGAACTTCGCCTTTCTCTCGGCCTTAGCGCCATTTTTTTGGGTGAGTGTTTTTTGGTTCCTGCGTTGGGATTCCGTGTACAATCCATAGACATCTGACCTCGGCACTTAGCATCATCACAGCAAACTAACTGTAGCCTTTCTCTCTTTCCCTGTAGAAACCTCTGCGCC'
 
 // Restriction enzyme recognition sites
-export const RESTRICTION_SITES = [
+export const RESTRICTION_SITES: RestrictionSite[] = [
   {
     enzyme: 'AatI',
     sequence: 'AGGCCT',
