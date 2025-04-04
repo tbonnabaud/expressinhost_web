ec3a5039f5db88576c220a3bbef8878654c209f1
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-04-04 10:34:34 +0200

    Fix tests

--- backend/tests/core/test_restriction_sites.py
+++ backend/tests/core/test_restriction_sites.py
@@ -1,7 +1,6 @@
 from backend.core.codon_tables import process_codon_table_from_file
 from backend.core.restriction_sites import (
     find_recognition_site_positions,
-    replace_codon_by_closest_rank,
     replace_first_codon_within_recognition_site,
 )
 
@@ -21,29 +20,18 @@ def test_find_recognition_site_positions():
 
 
 def test_replace_first_codon_within_recognition_site():
-    recognition_site = "AUGC"
-    sequence1 = "GGGGAUGCGGGGAUGCGGCGUACC"
-    recognition_site_positions = find_recognition_site_positions(
-        recognition_site, sequence1
-    )
-    expected_sequence = "GGGXXXGCGGGGXXXCGGCGUACC"
-
-    assert (
-        replace_first_codon_within_recognition_site(
-            sequence1, recognition_site_positions, lambda _: "XXX"
-        )
-        == expected_sequence
-    )
-
     host_codon_table = process_codon_table_from_file(
         "codon_tables/Bacillus_subtilis.csv", 0.5
     )
+    recognition_site = "UUGC"
+    sequence1 = "GGGGUUGCGGGGUUGCGGCGUACC"
+    recognition_site_positions = find_recognition_site_positions(
+        recognition_site, sequence1
+    )
 
     assert (
         replace_first_codon_within_recognition_site(
-            sequence1,
-            recognition_site_positions,
-            lambda x: replace_codon_by_closest_rank(x, host_codon_table),
+            sequence1, recognition_site_positions, host_codon_table
         )
-        != sequence1
+        == "GGGGUCGCGGGGCUCCGGCGUACC"
     )
