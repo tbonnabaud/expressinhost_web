ac191616ca3e90ad7bb833a56092b1e6a71584b4
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-04-14 13:53:46 +0200

    Rewrite functions and add dosctrings

--- backend/core/folding.py
+++ backend/core/folding.py
@@ -1,7 +1,9 @@
 import os
 import random
+from dataclasses import dataclass
+from typing import Iterable
 
-import pandas as pd
+import numpy as np
 from Bio.PDB import DSSP, PDBParser, ShrakeRupley
 
 from .codon_tables import ProcessedCodonTable, process_codon_table_from_file
@@ -56,7 +58,16 @@ REFERENCE_SASA = {
 }
 
 
-def extract_structure_info(pdb_filename: str):
+@dataclass
+class ResidueInfo:
+    residue_number: int
+    amino_acid: str
+    secondary_structure: str
+    sasa: np.float64
+    rsa: np.float64
+
+
+def extract_structure_infos(pdb_filename: str) -> list[ResidueInfo] | None:
     """
     Extract residue number, amino acid, secondary structure, SASA
     (solvent accessibility surface area) and RSA (relative SASA).
@@ -73,12 +84,7 @@ def extract_structure_info(pdb_filename: str):
         sr = ShrakeRupley()
         sr.compute(structure, level="S")
 
-        # storage lists
-        residue_numbers = []
-        amino_acids = []
-        secondary_structures = []
-        sasa_values = []
-        rsa_values = []
+        structure_infos = []
 
         for key in dssp.keys():
             chain_id, res_id = key
@@ -93,25 +99,19 @@ def extract_structure_info(pdb_filename: str):
                 # computing RSA
                 sasa = sum(atom.sasa for atom in residue if hasattr(atom, "sasa"))
                 ref_sasa = REFERENCE_SASA.get(aa, 100.0)
-                rsa = (sasa / ref_sasa) * 100
+                rsa = sasa / ref_sasa
 
-                # Append to lists
-                residue_numbers.append(resnum)
-                amino_acids.append(aa)
-                secondary_structures.append(ss)
-                sasa_values.append(sasa)
-                rsa_values.append(rsa)
+                structure_infos.append(
+                    ResidueInfo(
+                        residue_number=resnum,
+                        amino_acid=aa,
+                        secondary_structure=ss,
+                        sasa=sasa,
+                        rsa=rsa,
+                    )
+                )
 
-        # Create DataFrame
-        return pd.DataFrame(
-            {
-                "Residue Number": residue_numbers,
-                "Amino Acid": amino_acids,
-                "Secondary Structure": secondary_structures,
-                "SASA": sasa_values,
-                "RSA": rsa_values,
-            }
-        )
+        return structure_infos
 
     except Exception as e:
         print(f"Error processing {pdb_filename}: {str(e)}")
@@ -120,14 +120,38 @@ def extract_structure_info(pdb_filename: str):
 
 def select_codon_from_table(
     amino_acid: str, host_codon_table: ProcessedCodonTable, is_slow: bool
-):
+) -> str:
+    """
+    Selects a codon for a given amino acid from a codon table based on the desired speed.
+
+    This function filters the codon table to find codons that correspond to the specified
+    amino acid. It then selects a codon based on whether a slow or fast codon is desired.
+    If no codons are available for the desired speed, it selects the slowest fast codon
+    or the fastest slow codon as a fallback.
+
+    Parameters:
+        amino_acid (str): The single-letter code of the amino acid for which to select a codon.
+        host_codon_table (ProcessedCodonTable): A table containing codon information, including
+                                                amino acid, codon, and rank.
+        is_slow (bool): A flag indicating whether to select a slow codon (True) or a fast codon (False).
+
+    Returns:
+        str: The selected codon for the specified amino acid.
+
+    Notes:
+    - The function uses a rank to determine the speed of a codon, where a lower rank indicates
+      a slower codon and a higher rank indicates a faster codon.
+    - The function uses weighted random selection to choose a codon based on its rank.
+    - If no codons are available for the desired speed, the function falls back to the slowest
+      fast codon or the fastest slow codon.
+    """
     # Keep rows with a specific amino-acid
     filtered_rows = [
         row
         for row in host_codon_table.values()
+        # Use mapping to convert the single letter into to three letters
         if row.amino_acid == AMINO_ACID_LETTER_MAPPING[amino_acid]
     ]
-    # total_gcn = sum(map(lambda x: x.trna_gcn, filtered_rows))
 
     slow_rows = [row for row in filtered_rows if row.rank < 0.5]
     fast_rows = [row for row in filtered_rows if row.rank >= 0.5]
@@ -153,6 +177,41 @@ def select_codon_from_table(
     return random.choices(available_codons, weights=codon_weights)[0]
 
 
+def generate_mrna_from_structure_infos(
+    structure_infos: Iterable[ResidueInfo], rsa_threshold: float = 0.25
+) -> str:
+    """
+    Generates an mRNA sequence based on the provided structure information.
+
+    This function iterates over a collection of `ResidueInfo` objects, which contain
+    information about each residue in a protein structure. For each residue, it
+    determines whether the residue is considered "slow" based on its Relative Solvent
+    Accessibility (RSA) value. If the RSA is greater than the specified `rsa_threshold`,
+    the residue is marked as slow. The function then selects a codon for the residue's
+    amino acid from a host codon table, preferring codons that are less frequent
+    (indicating slower translation) if the residue is marked as slow. The selected
+    codons are concatenated to form the resulting mRNA sequence.
+
+    Args:
+        structure_infos (Iterable[ResidueInfo]): An iterable collection of `ResidueInfo`
+            objects, each containing information about a residue in the protein structure.
+        rsa_threshold (float): The threshold value for RSA above which a residue is
+            considered slow. Default is 0.25.
+
+    Returns:
+        str: The generated mRNA sequence as a string.
+    """
+
+    def generator():
+        for residue in structure_infos:
+            # RSA > rsa_threshold means outside, so considered as slow
+            is_slow = residue.rsa > rsa_threshold
+
+            yield select_codon_from_table(residue.amino_acid, host_codon_table, is_slow)
+
+    return "".join(generator())
+
+
 # Example usage
 if __name__ == "__main__":
     pdb_file = "examples/AF-P37330-F1-model_v4.pdb"
@@ -161,20 +220,12 @@ if __name__ == "__main__":
     )
 
     if os.path.exists(pdb_file):
-        df = extract_structure_info(pdb_file)
+        structure_infos = extract_structure_infos(pdb_file)
 
-        if df is not None:
-            # print(df.describe())
+        if structure_infos is not None:
+            print("".join([residue.amino_acid for residue in structure_infos]))
+            mrna_sequence = generate_mrna_from_structure_infos(structure_infos)
+            print(mrna_sequence)
 
-            for amino_acid, structure, rsa in zip(
-                df["Amino Acid"], df["Secondary Structure"], df["RSA"]
-            ):
-                # print(amino_acid, structure, rsa)
-                # RSA > to 25 means outside, so considered as slow
-                is_slow = rsa > 25
-                codon = select_codon_from_table(amino_acid, host_codon_table, is_slow)
-                print(f"{codon}, {is_slow=}")
-
-            # print(df)
     else:
         print(f"PDB file not found: {pdb_file}")
