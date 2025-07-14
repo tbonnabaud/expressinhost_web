a7dcf294c5d5867bbd42dc2640a205cc65d7fac7
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-07-14 16:03:35 +0200

    Add user documentation

--- frontend/src/views/CodonTableView.vue
+++ frontend/src/views/CodonTableView.vue
@@ -12,9 +12,40 @@ const user = store.currentUser
       <h1>tRNA GCN tables</h1>
       <hr />
 
+      <ul id="codonTableFormExplanations">
+        <li>
+          <strong>To create a table from scratch:</strong> select the table
+          "(None)"; input organism name; input table name; modify the values in
+          the amino acid boxes; click "Save as new".
+        </li>
+        <li>
+          <strong>To create a table from an existing one:</strong> select the
+          existing table; change the organism name and/or the table name; modify
+          the values in the amino acid boxes; click "Save as new".
+        </li>
+        <li>
+          <strong>To edit a custom table:</strong> select the custom table;
+          modify the values in the amino acid boxes; click "Update". Each logged
+          user can modify its own custom tables only. Default tables cannot be
+          modified.
+        </li>
+        <li><strong>To reset all fields:</strong> click "Reset".</li>
+        <li>
+          <strong>To delete a custom table:</strong> select it and click
+          "Delete".
+        </li>
+      </ul>
+
       <CodonTableForm />
     </template>
 
     <RequiredAuth v-else />
   </main>
 </template>
+
+<style scoped>
+#codonTableFormExplanations li {
+  font-style: italic;
+  list-style-type: disc;
+}
+</style>
