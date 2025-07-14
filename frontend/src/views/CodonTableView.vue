9007c7edaf8917148e76c3e301f862cf206310cc
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-07-14 16:07:43 +0200

    Use arrow

--- frontend/src/views/CodonTableView.vue
+++ frontend/src/views/CodonTableView.vue
@@ -15,19 +15,20 @@ const user = store.currentUser
       <ul id="codonTableFormExplanations">
         <li>
           <strong>To create a table from scratch:</strong> select the table
-          "(None)"; input organism name; input table name; modify the values in
-          the amino acid boxes; click "Save as new".
+          "(None)" &rarr; input organism name &rarr; input table name &rarr;
+          modify the values in the amino acid boxes &rarr; click "Save as new".
         </li>
         <li>
           <strong>To create a table from an existing one:</strong> select the
-          existing table; change the organism name and/or the table name; modify
-          the values in the amino acid boxes; click "Save as new".
+          existing table &rarr; change the organism name and/or the table name
+          &rarr; modify the values in the amino acid boxes &rarr; click "Save as
+          new".
         </li>
         <li>
-          <strong>To edit a custom table:</strong> select the custom table;
-          modify the values in the amino acid boxes; click "Update". Each logged
-          user can modify its own custom tables only. Default tables cannot be
-          modified.
+          <strong>To edit a custom table:</strong> select the custom table
+          &rarr; modify the values in the amino acid boxes &rarr; click
+          "Update". Each logged user can modify its own custom tables only.
+          Default tables cannot be modified.
         </li>
         <li><strong>To reset all fields:</strong> click "Reset".</li>
         <li>
