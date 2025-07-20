54dff4098e438c7b30ae2cb27ea91f362d264886
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-07-20 22:35:00 +0200

    Fix compute_similarity

--- backend/core/postprocessing.py
+++ backend/core/postprocessing.py
@@ -11,7 +11,7 @@ def compute_similarity(input_sequence: str, output_sequence: str) -> float:
     codon_nb = int(len(input_sequence) / 3)
 
     for t in range(codon_nb):
-        input_codon = input_sequence[3 * t : 3 * t + 3].replace("U", "T")
+        input_codon = input_sequence[3 * t : 3 * t + 3]
         output_codon = output_sequence[3 * t : 3 * t + 3]
 
         if input_codon == output_codon:
