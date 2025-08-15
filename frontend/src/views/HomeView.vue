a0256dd298e967c0d6ab91dfad9c6f0bc93ea675
Thierry Bonnabaud La Bruyère <Thierry.Bonnabaud.La.Bruyere@hhu.de>
2025-08-15 17:47:45 +0200

    Remove spaces

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -60,15 +60,13 @@ const user = store.currentUser
     <p>
       🅭 All results obtained from our softwares, e.g. by using this web
       interface, are licensed under the
-      <a href="https://creativecommons.org/licenses/by/4.0/legalcode.txt">
-        CC BY 4.0 terms
-      </a>
-      , including that attribution must be given to the creator, the
+      <a href="https://creativecommons.org/licenses/by/4.0/legalcode.txt"
+        >CC BY 4.0 terms</a
+      >, including that attribution must be given to the creator, the
       ExpressInHost team, by citing <a href="">our release article</a> and
       <a
         href="https://openresearchsoftware.metajnl.com/articles/10.5334/jors.385"
-      >
-        our previous work </a
+        >our previous work</a
       >.
     </p>
   </footer>
